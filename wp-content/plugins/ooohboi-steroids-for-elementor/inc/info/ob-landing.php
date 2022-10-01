9c2f8ad6d33c9298c555a1cf6cb4604a3f969561
unknown <romaritoxd@hotmail.com>
2022-10-01 09:24:23 -0700

    first commit

---
+++ wp-content/plugins/ooohboi-steroids-for-elementor/inc/info/ob-landing.php
@@ -0,0 +1,102 @@
+<div class="sfe-header">
+    <img src="<?php echo  SFE_DIR_URL . 'img/sfe-header.png'; ?>" alt="OoohBoi Steroids for Elementor" />
+</div>
+
+<div class="sfe-content">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-8">
+
+                <h3>What's new v2.0.1?</h3>
+                <p>
+                <strong>Fixes and updates mostly</strong>
+                </p>
+                <p>
+                Elementor 3.7 brought many changes that also affected Steroids for Elementor, so this release fixes many bugs caused by the deprecated functions. 
+                </p>
+                <p>
+                BREAKING BAD extension got a new option that allows you to set the Column's "Min-height". 
+                </p>
+                <p>
+                All of the GreenSock's animation libraries (and plugins) are now up to date! GSAP, ScrollTrigger, ScrollTo, and MotionPath have been "updated" to the latest 3.11
+                </p>
+                <p style="background-color: #ffe1e1; padding: 28px;">
+                PLEASE support this plugin development! Think about a small monthly donation if you find it useful for your business. Even $1 from you could have a big impact on development, new features and quicker bug fixes.<br/>
+                Thank you!
+                </p>
+                <p>&nbsp;</p>
+
+                <h3>What's new since v1.9.9?</h3>
+                <p>
+                <strong>3 brand new extensions: TABBR, COUNTERZ and POSTMAN</strong>
+                </p>
+                <p>
+                TABBR extension allows you to style up default Tabs widget to the max! This was the most outdated and neglected widget of Elementor and many people rather installed a replacement widget. 
+                From now on you no longer need anything else but the TABBR extension. 
+                </p>
+                <p>
+                    <img src="<?php echo  SFE_DIR_URL . 'img/tabbr2.png'; ?>" alt="Tabbr" />
+                </p>
+                <p>
+                Frustrated by the lack of options to the Counter widget? Sure you are. COUNTERZ extension will make it shine again.
+                </p>
+                <p>
+                Style up Text Editor or Posts widget content (headings, paragraph, lists, blockquotes). Basic styling, I know, but better than nothing, right?
+                </p>
+
+            </div>
+
+            <div class="col-4">
+
+                <h3>Sponsored by</h3>
+                <p>
+                    <img src="<?php echo  SFE_DIR_URL . 'img/RemPro.png'; ?>" alt="RemPro.nl" />
+                </p>
+                <p>
+                    <a href="mailto:oooh.boi29@gmail.com?subject=SFE%20Sponsorship">
+                    <img src="<?php echo  SFE_DIR_URL . 'img/sponsor-needed.png'; ?>" alt="Sponsor needed" />
+                    </a>
+                </p>
+
+                <hr />
+
+                <h3>Resources</h3>
+                <p>
+                Want to learn more about this plugin? Here's a list of tutorials on YouTube:
+                </p>
+                <ul>
+                    <li><a href="https://youtu.be/ccGpcvi-t4A" title="GSAP + Scroll Trigger + Locomotive Scroll" target="_blank">GSAP + Scroll Trigger + Locomotive Scroll</a></li>
+                    <li><a href="https://youtu.be/dG2uumcwX7s" title="The Smooth, Locomotive Scroll" target="_blank">The Smooth, Locomotive Scrolld</a></li>
+                    <li><a href="https://youtu.be/Vp8O_uVWD-c" title="The Hover Animator explained" target="_blank">The Hover Animator explained</a></li>
+                    <li><a href="https://youtu.be/JpRTDaJQQvE" title="Widget Stalker" target="_blank">How to optimize the layout in Elementor - Widget Stalker</a></li>
+                    <li><a href="https://youtu.be/WRPu1v4Kh9g" title="Masked Video Background" target="_blank">Masked Video Background - Videomasq</a></li>
+                    <li><a href="https://youtu.be/qS8lCzNhTbE" title="Teleporter Hero" target="_blank">Teleporter Hero</a></li>
+                    <li><a href="https://youtu.be/9t4vugF2Lok" title="Glider" target="_blank">Glider - Custom Content Slider for Elementor</a></li>
+                </ul>
+                <p class="small-text"> More videos will be added soon!</p>
+
+                <hr />
+
+                <h3>Donations</h3>
+                <p>
+                If you think that this plugin helps your business, please consider a small donation to support further development. Thank you! 
+                </p>
+                <p>
+                    <a href="https://www.paypal.me/ooohboi" title="Donate with PayPal" target="_blank"><img src="<?php echo  SFE_DIR_URL . 'img/paypal-donate.png'; ?>" alt="Donate with PayPal" class="pp" /></a>
+                </p>
+
+            </div>
+
+            <div class="col-12">
+                <hr />
+                <p class="small-text">Current version: <?php echo OoohBoi_Steroids::VERSION; ?></p>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
